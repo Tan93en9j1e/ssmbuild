6187959cea6e8bff5145932fb50330da1ee0fc43
tmj <3124894818@qq.com>
2026-05-08 15:06:22 +0800

    refactor(spring): 重构Spring配置以分离DAO和服务层
    - 在applicationContext.xml中导入spring-service.xml和spring-dao.xml
    - 将BookServiceImpl类上的@Service注解移除并调整依赖注入方式
    - 新增spring-dao.xml配置文件用于数据库连接池和MyBatis配置
    - 新增spring-service.xml配置文件用于服务层组件扫描和事务管理
    - 分离数据访问层和服务层的配置逻辑，提高模块化程度

--- src/main/java/com/tang/service/BookServiceImpl.java
+++ src/main/java/com/tang/service/BookServiceImpl.java
@@ -2,6 +2,8 @@ package com.tang.service;
 
 import com.tang.dao.BookMapper;
 import com.tang.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -14,8 +16,10 @@ import java.util.List;
  * @Date: 2026/5/8 14:30
  * @Version: 1.0
  */
+//@Service
 public class BookServiceImpl implements BookService {
     // 调用dao层
+//    @Autowired
     private BookMapper bookMapper;
 
     public void setBookMapper(BookMapper bookMapper) {
